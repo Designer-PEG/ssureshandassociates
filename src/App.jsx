d51a8241ac433a7cdc9203318d1bede4fdef54c5
Designer-PEG <designer.professionaledgeglobal@gmail.com>
2025-07-23 13:34:57 +0545

    for error duplication

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './index.css';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/sureshandassociates">
       <div className="min-h-screen flex flex-col">
         <ScrollToTop />
         <Header />
